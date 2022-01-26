fd57579950661397bd6bf6ad04dc4e3861625299
Charles <cpl17@pitt.edu>
2022-01-26 12:31:21 -0500

    Update styles path

--- main.py
+++ main.py
@@ -7,9 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/projects/')
-def projects():
-    return render_template('projects/DNA.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
